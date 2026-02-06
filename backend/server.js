513b2233008587bb533d9f978bf685a550078967
Muktadir-Ashir <ashirahmed04115@gmail.com>
2026-02-06 15:32:55 +0600

    Product Management (Backend)

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
-import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import express from 'express';
 import connectDB from './config/db.js';
 import authRoutes from './routes/authRoutes.js';
+import productRoutes from './routes/productsRoutes.js';
 
 
 dotenv.config();
@@ -12,6 +13,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api/auth', authRoutes);
+app.use('/api/products', productRoutes);
 
 
 
