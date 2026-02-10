8d9d815c478e0d6726c7b69b27247c45ac6c82f3
Muktadir-Ashir <ashirahmed04115@gmail.com>
2026-02-10 13:01:32 +0600

    Implement cart functionality with add, remove, update, and retrieve operations

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import express from 'express';
 import connectDB from './config/db.js';
 import authRoutes from './routes/authRoutes.js';
 import productRoutes from './routes/productsRoutes.js';
+import cartRoutes from './routes/cart.js';
 
 
 dotenv.config();
@@ -14,6 +15,8 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/products', productRoutes);
+app.use('/cart', cartRoutes);
+
 
 
 
