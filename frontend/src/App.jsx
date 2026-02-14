4ecd7c40b62991dac33a3813817cb2e4c10f312b
Muktadir-Ashir <ashirahmed04115@gmail.com>
2026-02-14 13:28:34 +0600

    Add cart functionality with cart routes, cart page, and navbar integration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, RouterProvider } from "react-router";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
@@ -6,19 +6,36 @@ import ProductDetails from "./pages/ProductDetails";
 import AddProduct from "./admin/AddProduct";
 import ProductList from "./admin/ProductList";
 import EditProduct from "./admin/EditProduct";
+import Navbar from "./components/Navbar";
+import Cart from "./pages/Cart";
+
+function Layout() {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  );
+}
 
 
 
 const router = createBrowserRouter([
-  
-      { path: "/", element: <Home /> },
+  {
+    element: <Layout />,
+    children: [
+            { path: "/", element: <Home /> },
       { path: "/login", element: <Login /> },
       { path: "/signup", element: <Signup /> },
       { path: "/product/:id", element: <ProductDetails /> },
+      { path : "/cart", element: <Cart />},
 
       { path: "/admin/products/add", element: <AddProduct /> },
       { path: "/admin/products", element: <ProductList /> },
       { path: "/admin/products/update/:id", element: <EditProduct /> },
+    ],
+  },
+
 ]);
 
 export default function App() {
